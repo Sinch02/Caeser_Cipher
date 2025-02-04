64745745edf90d1155465d03f26ad06bb2772499
Sinchana B R <109731549+Sinch02@users.noreply.github.com>
2025-02-04 08:43:07 +0000

    Completed project

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def vigenere(message, key, direction=1):
     final_message = ''
 
     for char in message.lower():
-    
+
         # Append any non-letter character to the message
-        if not char.isaplha():
+        if not char.isalpha():
             final_message += char
         else:        
             # Find the right key character to encode/decode
@@ -19,21 +19,18 @@ def vigenere(message, key, direction=1):
             # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset * direction) % len(alphabet)
+            new_index = (index + offset*direction) % len(alphabet)
             final_message += alphabet[new_index]
     
     return final_message
 
 def encrypt(message, key):
-    return vigenere(message,key)
-
-def decrypt(message, key):
-    return vigenere(message,key,-1)   
+    return vigenere(message, key)
     
+def decrypt(message, key):
+    return vigenere(message, key, -1)
 
-print(f'Encrypted text: {text}')
+print(f'\nEncrypted text: {text}')
 print(f'Key: {custom_key}')
 decryption = decrypt(text, custom_key)
 print(f'\nDecrypted text: {decryption}\n')
-
-
